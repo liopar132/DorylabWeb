e2b6048c3deb49c5bf5311b4bc8cd30441fd1517
Schrodingdong <hamzaalamiibnjamaa123456789@gmail.com>
2024-07-25 17:45:43 +0100

    Add description section

--- src/components/description/index.jsx
+++ src/components/description/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import Ac from './air-conditioners-7558725_1280.jpg'
 
 function Description() {
   return (
     <div className='container min-w-full py-4 px-10'>
-      <div className='container mx-auto '>
+      <div className='container mx-auto text-center	flex flex-col items-center'>
         <h1 className='text-5xl font-bold py-5'>Qui Sommes-Nous ?</h1>
-        <p>Dorylab Rabat est une société spécialisée dans les domaines de la climatisation et de la réfrigération avec plus de 25 ans d’expérience dans le secteur. Nous nous spécialisons dans la réparation, l’installation et l’entretien d’équipements de climatisation , domestiques, commerciaux et industriels, apportant un savoir faire technique. Explorez notre site Web, pour en apprendre d’avantage sur nos services et consultez notre offre de climatisation. Si vous avez un projet en tête ou besoin d’un service, n’hésiter pas à nous contacter car nous vous assurons que nous sommes la société la plus professionnel et compétitif dans la ville de Rabat.</p>
+        <h2 className='text-3xl font-thin'>Experts en climatisation et ventilation depuis plus de 25 ans.</h2>
+        <br/>
+        <p className='text-justify center max-w-7xl' style={{"textAlignLast" : "center"}}>Dorylab Rabat est une société spécialisée dans les domaines de la climatisation et de la réfrigération avec plus de 15 ans d’expérience dans le secteur. Nous nous spécialisons dans la réparation, l’installation et l’entretien d’équipements de climatisation ,  domestiques, commerciaux et industriels, apportant un savoir faire technique.</p>
+        <br/>
+        <br/>
+        <img src={Ac} alt="dorylab pics" srcset={Ac} className='max-w-xl w-3/5 rounded-xl mx-auto shadow-md'/>
       </div>
+      <br/>
+      <br/>
     </div>
   )
 }
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,8 +3,8 @@ import dorylab_logo from './dorylab.png'
 
 function Navbar() {
   return (
-    <div className='container min-w-full py-4 px-10 shadow-md bg-white absolute top-0 left-0'>
-      <div className='container mx-auto flex flex-row justify-between items-center'>
+    <div className='container min-w-full py-2 px-10 absolute  top-0 left-0'>
+      <div className='container mx-auto py-4 px-10 my-5 shadow-2xl flex flex-row justify-between bg-white items-center rounded-xl'>
           <img src={dorylab_logo} className='w-12'/>
           <menu className='h-fit flex flex-row justify-between gap-6'>
             <a className='h-fit' href='https://www.google.com'>Services</a>
