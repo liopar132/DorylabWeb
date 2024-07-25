85215457a94f999d854e4ef7e5d18398e8953da3
liopar123 <oussamabenom@gmail.com>
2024-07-25 21:09:28 +0200

    lolita

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/navbar';
 import Header from './components/header'
 import Description from './components/description';
 import Footer from './components/footer';
+import Services from './components/services';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar></Navbar>
       <Header></Header>
       <Description></Description>
+      <Services></Services>
       <Footer></Footer>
     </div>
   );
