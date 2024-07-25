9a96e521660b89addb15969ab37a73d7ce6f9bfe
Schrodingdong <hamzaalamiibnjamaa123456789@gmail.com>
2024-07-25 18:25:49 +0100

    Add partners

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/navbar';
 import Header from './components/header'
 import Description from './components/description';
 import Footer from './components/footer';
+import Partners from './components/partners';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar></Navbar>
       <Header></Header>
       <Description></Description>
+      <Partners></Partners>
       <Footer></Footer>
     </div>
   );
