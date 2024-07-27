861337c625d691abcd373aaf828a939eb9f9ec27
Schrodingdong <hamzaalamiibnjamaa123456789@gmail.com>
2024-07-27 21:46:00 +0100

    More warning fixes

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -10,9 +10,9 @@ function Footer() {
         <div className='container mx-auto w-fit flex flex-col gap-4 items-center pt-5'>
             <img src={dorylab_logo} alt="dorylabe logo" srcset={dorylab_logo} className='size-16' />
             <div className='flex flex-row gap-10'>
-                <a target="_blank" rel='noopener' href='https://www.facebook.com/dorylabmaroc/'><Instagram fontSize='large' color='white'/></a>
-                <a target="_blank" rel='noopener' href='https://www.facebook.com/dorylabmaroc/'><Facebook fontSize='large' color='white'/></a>
-                <a target="_blank" rel='noopener' href='https://www.facebook.com/dorylabmaroc/'><Google fontSize='large' color='white'/></a>
+                <a target="_blank" rel="noreferrer" href='https://www.facebook.com/dorylabmaroc/'><Instagram fontSize='large' color='white'/></a>
+                <a target="_blank" rel="noreferrer" href='https://www.facebook.com/dorylabmaroc/'><Facebook fontSize='large' color='white'/></a>
+                <a target="_blank" rel="noreferrer" href='https://www.facebook.com/dorylabmaroc/'><Google fontSize='large' color='white'/></a>
             </div>
             <p className='font-thin'>Copyright Dorylab - 2024</p>
         </div>
