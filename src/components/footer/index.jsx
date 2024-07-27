4b1ad3de9ec6af27eaa51241301a4c12689a7b64
Schrodingdong <hamzaalamiibnjamaa123456789@gmail.com>
2024-07-27 22:10:17 +0100

    Parteners ui fix

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,9 +4,8 @@ import { Facebook, Google, Instagram } from '@mui/icons-material'
 
 function Footer() {
   return (
-    <div className='container min-w-full py-10 px-10  bg-blue-500 text-white'>
+    <div className='container min-w-full py-10 px-10 text-white'>
       <div className='container mx-auto'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate corporis quo voluptatum. Libero, reprehenderit illo, quaerat repellat modi maiores cumque neque iusto laborum, labore beatae commodi in. Excepturi, quod quas.
         <div className='container mx-auto w-fit flex flex-col gap-4 items-center pt-5'>
             <img src={dorylab_logo} alt="dorylabe logo" srcset={dorylab_logo} className='size-16' />
             <div className='flex flex-row gap-10'>
--- src/components/partners/index.jsx
+++ src/components/partners/index.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import carrier_logo from './carrier.png'
 import lg_logo from './LG-Logo.png'
 import fitco_logo from './fitco.png'
+import panasonic_logo from './panasonic.png'
+import toshiba_logo from './Toshiba-logo.png'
 
 function Partners() {
   return (
-    <div className='container min-w-full py-4 px-10' >
-      <div className='container max-w-5xl mx-auto text-center	flex flex-col items-center'>
+    <div className='container min-w-full my-16 py-4 px-10 bg-white shadow-xl' >
+      <div className='container max-w-7xl mx-auto text-center	flex flex-col'>
         <h1 className='text-5xl font-bold pb-5'>Nos Partenaires</h1>
-        <div className='flex flex-row justify-between w-2/4'>
+        <div className='flex flex-row justify-around'>
           <img className='size-28 object-contain' src={lg_logo} alt="" />
           <img className='size-28 object-contain' src={carrier_logo} alt="" />
           <img className='size-28 object-contain' src={fitco_logo} alt="" />
+          <img className='size-28 object-contain' src={panasonic_logo} alt="" />
+          <img className='size-28 object-contain' src={toshiba_logo} alt="" />
         </div>
       </div>
     </div>
